432192d17f4bdc070ce8d7af3fb872fec0379fd8
Departamento Viaticos <viaticos@sada>
2023-09-18 12:28:33 -0300

    agregué el modulo1.js

---
+++ modulo1.js
@@ -0,0 +1,87 @@
+                   //VARIABLES
+ 
+// var nombre = "Facundo"; //De alcance, o rango global.
+// var apellido = "Diaz";
+
+// let nombre = "Facundo";
+// let apellido = "Diaz";   //De alcance local.
+
+                    //PROMPT
+
+// let nombre = prompt ("decime tu nombre");
+// alert ("Hola " + nombre)
+
+                    //OPERADORES DE ASIGNACION (asigna valor basado en el operando de la izq)
+
+// let num1 = 3;
+// let num2 = 4;
+// let num3 = 5;
+
+// num1 += 3;
+// num2 *= 1;
+// num3 %= 5;
+// num2 **= 2;
+// document.write (num2)
+
+                    //OPERADORES ARITMETICOS (toman valores numericos, ya sean literales o variables,
+                    //como sus operandos y retornan UN VALOR NUMERICO)
+
+
+// let num1 = 3;
+// let num2 = 4;
+// let num3 = 5;
+// let num4 = 6;
+
+// let resultado = num1 + num2;
+// let resultado = num1 - num3;
+// let resultado = num1 / num3;
+// let resultado = num1  ** num1;
+// let resultado = num4 % num2;
+
+// num1++;
+// let resultado = num1
+
+// num1--;
+
+// //resultado = -num4;
+// document.write (num1)
+
+
+                    //CONCATENACION
+
+
+// let obj1 = "bufanda";
+// let obj2 = "campera";
+// let obj3 = "jean";
+// let num1 = 2;
+// let num2 = 4;
+// let num3 = 6;
+// let texto1 = "bienvenido";
+// let texto2 = "saludo";
+// let texto3 = "respuesta";
+
+
+// let frase = obj1 + " con " + obj3;
+// document.write(suma)
+
+// let frase = "Hola! " + texto1 + " a tu " + texto3;
+// document.write (frase)
+
+// let frase = "" + num1 + num2;
+// document.write (frase)
+
+//CONCAT: solo para strings, es un metodo de cadena.
+// let frase = bienvenido.concat (num1)
+// document.write (frase)
+
+// let frase = `Hola! ${texto1} a tu pagina de ${texto3}`;
+// document.write (frase)
+
+                     //OPERADORES LOGICOS (devuelven BOOLEAN, a partir de cierta condicion)
+
+// let num1 = 3;
+// let num2 = 4;
+// let num3 = 1;
+
+// let afirmacion = num1 > num2 ;
+
